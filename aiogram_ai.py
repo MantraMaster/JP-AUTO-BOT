f7fe907dfb0d3a32fbf0c15f27f18e51486ac70a
Дмитрий Поляков <35742593+Yanisselection@users.noreply.github.com>
2024-12-01 15:08:31 +0300

    success

--- aiogram_ai.py
+++ aiogram_ai.py
@@ -1,5 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.client import telegram
 from aiogram.filters import CommandStart
 import mistral
 
@@ -15,19 +16,32 @@ async def start_cmd(message: types.Message):
         print(mistral.history)
     else:
         print("Уже существует", {message.from_user.id})
-    await message.answer('Как к Вам обращаться?')
+    await message.answer('Как к вам обращаться?')
+
 
 @dp.message()
 async def question(message: types.Message):
+    # Проверка, является ли чат групповым
+    if message.chat.type != "private":
+        return  # Игнорировать сообщения в групповых чатах
+
     if message.from_user.id not in mistral.history:
         id_user = message.from_user.id
         mistral.history[id_user] = [{"role": "system", "content": mistral.content_edu}]
         print(mistral.history)
-    print(message.from_user.username,"|", message.text)
+    print(message.from_user.username, "|", message.text)
     id_user = message.from_user.id
     mistral.question = message.text
     response = mistral.get_chat_response(id_user, message.from_user.username, message.text)
+    await bot.send_chat_action(chat_id=id_user, action="typing")
+    await asyncio.sleep(2)
     await message.answer(response)
+    await bot.send_message(chat_id=-1002303909150,
+                            text=f"<b>💬 Сообщение от пользователя {message.from_user.id, message.from_user.username}</b>"
+                                 f": \n{message.text}", parse_mode="HTML")
+
+
+
 
 async def main() -> None:
     await dp.start_polling(bot)
